955f08a9949bf9458ccf0f2859c9390e3181d3f4
Randy McMillan <randy.lee.mcmillan@gmail.com>
2018-02-07 22:19:39 -0500

    Changes to be committed: 	deleted:    minergate_ethminer_opencl-etc_etc.bat

--- minergate_ethminer_opencl-etc_etc.bat
+++
@@ -1,6 +0,0 @@
-set GPU_FORCE_64BIT_PTR=0
-set GPU_MAX_HEAP_SIZE=100
-set GPU_USE_SYNC_OBJECTS=1
-set GPU_MAX_ALLOC_PERCENT=100
-set GPU_SINGLE_ALLOC_PERCENT=100
-./ethminer -G -S etc.pool.minergate.com:45777 -u randy.lee.mcmillan@gmail.com
