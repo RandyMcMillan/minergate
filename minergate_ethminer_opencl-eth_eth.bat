f206d489d04aa053c4c5658cd0ba626fcf088e08
Randy McMillan <randy.lee.mcmillan@gmail.com>
2018-02-07 22:18:21 -0500

    Changes to be committed: 	modified:   minergate_ethminer_opencl-eth_eth.bat
     Untracked files:
    	minergate_ethminer_opencl-etc_etc.bat
    	minergate_minergate_cpu_cli.bat

--- minergate_ethminer_opencl-eth_eth.bat
+++ minergate_ethminer_opencl-eth_eth.bat
@@ -3,4 +3,4 @@ set GPU_MAX_HEAP_SIZE=100
 set GPU_USE_SYNC_OBJECTS=1
 set GPU_MAX_ALLOC_PERCENT=100
 set GPU_SINGLE_ALLOC_PERCENT=100
-~/bin/./ethminer -G -S eth.pool.minergate.com:45791 -u randy.lee.mcmillan@gmail.com
+./ethminer -G -S eth.pool.minergate.com:45791 -u randy.lee.mcmillan@gmail.com
